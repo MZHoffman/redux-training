fdec0d05bbb1868cb2ada1dc77d36cf73fe3d2d4
Michal Hoffman <michal.z.hoffman@gmail.com>
2023-02-21 12:30:45 +0000

    Bux fixes

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,6 @@
 import { createStore } from 'redux'
 
-counterReducer = (state = { counter: 0 }, action) => {
+const counterReducer = (state = { counter: 0 }, action) => {
   if (action.type === 'increnet') {
     return { counter: state.counter + 1 }
   }
