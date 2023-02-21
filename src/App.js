64392403edbe2764aa3375bc59d662b4936b9d48
Michal Hoffman <michal.z.hoffman@gmail.com>
2023-02-21 15:24:38 +0000

    splitting reux reducers in separate files

--- src/App.js
+++ src/App.js
@@ -8,8 +8,6 @@ import UserProfile from './components/UserProfile'
 
 function App() {
   const auth = useSelector((state) => state.auth.isAuthenticated)
-  console.log(auth)
-
   return (
     <Fragment>
       <Header />
