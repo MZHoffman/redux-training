6acfbb0077fd7949ba5d10840eb4952cf9a86efa
Michal Hoffman <michal.z.hoffman@gmail.com>
2023-02-21 13:34:51 +0000

    toggling counter visability

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -5,7 +5,10 @@ import classes from './Counter.module.css'
 const Counter = () => {
   const dispatch = useDispatch()
   const counter = useSelector((state) => state.counter)
-  const toggleCounterHandler = () => {}
+  const show = useSelector((state) => state.showCounter)
+  const toggleCounterHandler = () => {
+    dispatch({ type: 'toggle' })
+  }
 
   const incrementHandler = () => {
     dispatch({ type: 'increment' })
@@ -13,13 +16,17 @@ const Counter = () => {
   const decrementHandler = () => {
     dispatch({ type: 'decrement' })
   }
+  const increaseHandler = () => {
+    dispatch({ type: 'increase', amount: 5 })
+  }
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>{counter}</div>
+      {show && <div className={classes.value}>{counter}</div>}{' '}
       <div>
         <button onClick={incrementHandler}>Increnet</button>
+        <button onClick={increaseHandler}>Increase by 5</button>
         <button onClick={decrementHandler}>Decrenet</button>
       </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
